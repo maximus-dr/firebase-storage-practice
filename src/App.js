b480e96bd113bcdbf65e5ccee0c44ced7b227a0b
Max Ivanov <maximus-dr@mail.ru>
2021-01-20 15:39:40 +0300

    Realized image upload and submit to firebase storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { app } from './base';
+import React, { useEffect, useState } from 'react';
+
+const db = app.firestore();
+
 
 function App() {
+  const [fileURL, setFileURL] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  const onFileChange = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+
+    await fileRef.put(file);
+    setFileURL(await fileRef.getDownloadURL());
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+
+    if (!username) {
+      return;
+    }
+
+    db.collection('users').doc(username).set({
+      name: username,
+      avatar: fileURL
+    });
+  }
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersCollection = await db.collection('users').get();
+      setUsers(usersCollection.docs.map(doc => {
+        return doc.data();
+      }));
+    }
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="file" onChange={onFileChange} />
+        <input type="text" name="username" placeholder="Name" />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {users.map(user => {
+          return (
+            <li key={user.name}>
+              <img src={user.avatar} alt={user.name} width="100" height="100" />
+              <p>{user.name}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
